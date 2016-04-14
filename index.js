f7282c505a1703fc5b3cba00cefffc5726720552
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2016-04-14 21:40:02 +0900

    add reminder

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
+const moment = require("moment")
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
-const PORT = 8080
+const kue = require("kue")
 const createRequestData = require("./lib/create-request-data")
 const say = require("./lib/say")
 
-app.use(bodyParser.json())
+const PORT = 8080
+const PATTERNS = ["やばい", "わろた"]
 
-app.post('/', function (req, res) {
-  console.log("received")
-  req.body.result.forEach(function(res) {
-    console.log(res)
-    const data = createRequestData("Hallo", [res.content.from])
-    say(data)
-  })
-  res.send(':)')
+const queue = kue.createQueue({
+  redis: require("./config/redis")
 })
 
+queue.process("say", function(job, ctx, done) {
+  console.log("processed say")
+  say(job.data.requestData)
+  done()
+})
+
+const app = express()
+
+app.use(bodyParser.json())
+app.post('/', function (req, res) {
+  console.log("received message")
+  req.body.result.forEach(function(res) {
+    resTextArguments = res.content.text.split(" ")
+    const from = res.content.from
+    const date = moment(resTextArguments.slice(0, -1).join(" "))
+    const message = resTextArguments[resTextArguments.length - 1]
+
+    if(!date.isValid()) {
+      queue.create("say", {
+        requestData: createRequestData(PATTERNS[Math.random() * PATTERNS.length | 0], [from]),
+      }).save()
+      return
+    }
+
+    queue.create("say", {
+      requestData: createRequestData(`${date.format("YYYY/MM/DD HH:mm:ss")}\nになったら声かけますね`, [from]),
+    }).save()
+
+    queue.create("say", {
+      requestData: createRequestData(message, [from]),
+    }).delay(date.toDate()).save()
+  })
+
+  res.send(':)')
+})
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`start listening on port ${PORT}`)
 })
