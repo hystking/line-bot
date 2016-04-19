25174f3759219ac48b02383511bde1749c3ca112
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2016-04-20 03:14:29 +0900

    ブレスト

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const moment = require("moment")
+const _ = require("lodash")
 const express = require('express')
 const bodyParser = require('body-parser')
 const kue = require("kue")
-const createRequestData = require("./lib/create-request-data")
+const redux = require("redux")
+const actionTypes = require("./lib/action-types")
+const combineReducers = redux.combineReducers
+const createStore = redux.createStore
 const say = require("./lib/say")
 
 const PORT = 8080
@@ -14,37 +17,57 @@ const queue = kue.createQueue({
 
 queue.process("say", function(job, ctx, done) {
   console.log("processed say")
-  say(job.data.requestData)
-  done()
+  say(job.data.requestData, function(){
+    if(job.data.next) {
+      queueMessage(job.data.next)
+    }
+    done()
+  })
 })
 
 const app = express()
+const store = createStore(
+  combineReducers({
+    talks: require("./lib/reducers/talks")
+  })
+)
+
+function queueMessage(message) {
+  const q = queue.create("say", message)
+  if(message.delay) {
+    q.delay(message.delay)
+  }
+  q.save()
+}
+
+function processMessages(){
+  console.log("proceeess")
+  const state = store.getState()
+  const messages = _.flatten(_.map(state.talks, function(talk){
+    return talk.messages
+  }))
+  if(messages.length === 0) {
+    return
+  }
+  messages.forEach(queueMessage)
+  store.dispatch({
+    type: actionTypes.CLEAR_ALL_MESSAGES,
+  })
+}
+
+store.subscribe(processMessages)
 
 app.use(bodyParser.json())
 app.post('/', function (req, res) {
   console.log("received message")
   req.body.result.forEach(function(res) {
-    resTextArguments = res.content.text.split(" ")
-    const from = res.content.from
-    const date = moment(resTextArguments.slice(0, -1).join(" "))
-    const message = resTextArguments[resTextArguments.length - 1]
-
-    if(!date.isValid()) {
-      queue.create("say", {
-        requestData: createRequestData(PATTERNS[Math.random() * PATTERNS.length | 0], [from]),
-      }).save()
-      return
-    }
-
-    queue.create("say", {
-      requestData: createRequestData(`${date.format("YYYY/MM/DD HH:mm:ss")}\nになったら声かけますね`, [from]),
-    }).save()
-
-    queue.create("say", {
-      requestData: createRequestData(message, [from]),
-    }).delay(date.toDate()).save()
+    store.dispatch({
+      type: actionTypes.RECEIVE_MESSAGE,
+      from: res.content.from,
+      text: res.content.text,
+      random: Math.random(),
+    })
   })
-
   res.send(':)')
 })
 app.listen(PORT, function () {
