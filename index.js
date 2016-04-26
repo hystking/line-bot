591a78890ee43aea434b8da636c66ece6028c6b3
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2016-04-27 00:21:41 +0900

    use docomo dialogue api

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const redux = require("redux")
 const actionTypes = require("./lib/action-types")
 const combineReducers = redux.combineReducers
 const createStore = redux.createStore
+const createRequestData = require("./lib/create-request-data")
 const say = require("./lib/say")
+const dialogue = require("./lib/dialogue")
 
 const PORT = 8080
 
@@ -20,6 +22,16 @@ queue.process("say", function(job, ctx, done) {
     if(job.data.next) {
       queueMessage(job.data.next)
     }
+    done()
+  })
+})
+
+queue.process("dialogue", function(job, ctx, done) {
+  console.log("processed dialogue")
+  dialogue(job.data.requestData, function(res){
+    queueMessage({
+      requestData: createRequestData(res.utt, job.data.to),
+    })
     done()
   })
 })
@@ -39,8 +51,13 @@ function queueMessage(message) {
   q.save()
 }
 
+function queueDialogue(message) {
+  const q = queue.create("dialogue", message)
+  q.save()
+}
+
 function processMessages(){
-  console.log("proceeess")
+  console.log("proceeess message")
   const state = store.getState()
   const messages = _.flatten(_.map(state.talks, function(talk){
     return talk.messages
@@ -54,7 +71,23 @@ function processMessages(){
   })
 }
 
+function processDialogues(){
+  console.log("process dialogues")
+  const state = store.getState()
+  const dialogues = _.flatten(_.map(state.talks, function(talk){
+    return talk.dialogues
+  }))
+  if(dialogues.length === 0) {
+    return
+  }
+  dialogues.forEach(queueDialogue)
+  store.dispatch({
+    type: actionTypes.CLEAR_ALL_DIALOGUES,
+  })
+}
+
 store.subscribe(processMessages)
+store.subscribe(processDialogues)
 
 app.use(bodyParser.json())
 app.post('/', function (req, res) {
